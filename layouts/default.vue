05f894daddf97ee453419694babe650bada7dff1
kayo289 <kayokapi@icloud.com>
2019-11-02 18:27:09 +0900

    一覧表示

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,9 +26,6 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,37 @@
 <template>
-  <v-layout column justify-center align-center>
-    <!-- <v-flex xs12 sm8 md6> -->
-    <div class="text-center">
-      <ul id="titles">
-        <li v-for="(item, index) in items" :key="index">
-          {{ item.title }}
-        </li>
-      </ul>
-    </div>
-    <!-- </v-flex> -->
+  <v-layout column justify-center>
+    <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
+      <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="text in texts" :key="text">
+        <v-card v-for="(t, key, index) in text" :key="index">
+          <v-card-text>
+            {{ t }}
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+    <!-- <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item.title }}</li>
+    </ul> -->
   </v-layout>
 </template>
 
 <script>
 export default {
   data() {
+    const init = [92, 88, 64, 86]
+    const good = ['aa', 'bb', 'cc']
     return {
-      items: [
+      titles: [
         { title: '桃太郎' },
         { title: 'delta大冒険' },
         { title: '神々の誕生' }
-      ]
+      ],
+      tab: null,
+      items: ['新着順', 'いいね順'],
+      texts: [init, good]
     }
   }
 }
