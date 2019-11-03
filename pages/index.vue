1b7320c75dceb7f7bcd4f3d8940874a94772ecef
ryota misumi <3delta7@gmail.com>
2019-11-03 11:50:47 +0900

    firestoreでデータ取得する

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,23 @@
     <v-tabs v-model="tab" background-color="transparent" grow>
       <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
     </v-tabs>
+    {{ novels }}
+    {{ numbera }}
     <v-tabs-items v-model="tab">
       <v-tab-item v-for="text in texts" :key="text">
         <v-container>
           <v-row>
             <v-col
+              v-for="(t, key, index) in text"
+              :key="index"
               cols="12"
               sm="6"
               md="4"
-              v-for="(t, key, index) in text"
-              :key="index"
               class="d-flex"
             >
               <v-card width="100%">
-                <v-card-title>
-                  Unlimited music now
-                </v-card-title>
-                <v-card-text>
-                  {{ t }}
-                </v-card-text>
+                <v-card-title>Unlimited music now</v-card-title>
+                <v-card-text>{{ t }}</v-card-text>
               </v-card>
             </v-col>
           </v-row>
@@ -32,6 +30,7 @@
 </template>
 
 <script>
+import firebase from '~/plugins/firebase'
 export default {
   data() {
     const init = [911111, 88, 64, 86, 111111, 5555]
@@ -42,30 +41,26 @@ export default {
       'ddddddd'
     ]
     return {
-      datas: [
-        {title: '桃太郎',
-         content: [
-           {detail: "dafadfa"},
-           {detail: "daaaaaa"}
-         ]
-        },
-        {title: 'delta大冒険',
-         content: [
-           {detail: "dafadfa"},
-           {detail: "daaaaaa"}
-         ]
-        },
-        {title: '神々',
-         content: [
-           {detail: "dafadfa"},
-           {detail: "daaaaaa"}
-         ]
-        }
-      ],
+      novels: [''],
       tab: null,
       items: ['新着順', 'いいね順'],
-      texts: [init, good]
+      texts: [init, good],
+      numbera: 1
     }
+  },
+  mounted() {
+    this.numbera = 2
+    const db = firebase.firestore()
+    const arrays = []
+    db.collection('novel')
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          // this.novels = doc.data()
+          arrays.push(doc.data())
+        })
+      })
+    this.novels = arrays
   }
 }
 </script>
