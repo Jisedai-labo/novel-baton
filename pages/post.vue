d1c282bc828d20fe53bf756814dbaa36b22b940d
Masato Kiryu <kiri9120@gmail.com>
2019-11-03 14:34:12 +0900

    Update post.vue

--- pages/post.vue
+++ pages/post.vue
@@ -1,15 +1,12 @@
 <template>
   <v-card class="mx-auto py-6">
-    <v-card-title class="display-2">
-      新規投稿
-    </v-card-title>
+    <v-card-title class="display-2">新規投稿</v-card-title>
     <v-card-text>
       <p>作成日：2019/11/02</p>
-      <p class="display-1 text--primary">
-        タイトル
-      </p>
+      <p class="display-1 text--primary">タイトル</p>
       <div class="text--primary">
         <v-text-field
+          v-model="title"
           :rules="titleRules"
           label="20文字以内で入力してね"
           required
@@ -17,11 +14,10 @@
         >
         </v-text-field>
       </div>
-      <p class="display-1 text--primary mt-6">
-        文章
-      </p>
+      <p class="display-1 text--primary mt-6">文章</p>
       <div class="text--primary">
         <v-textarea
+          v-model="content"
           :rules="postRules"
           label="140文字以内で入力してね"
           rows="10"
@@ -31,16 +27,23 @@
       </div>
     </v-card-text>
     <v-card-actions class="justify-center">
-      <v-btn raised large color="grey lighten-1 accent-4">
-        戻る
-      </v-btn>
-      <v-btn raised large color="primary accent-4">
+      <v-btn raised large color="grey lighten-1 accent-4">戻る</v-btn>
+      <v-btn raised large color="primary accent-4" @click="post">
         作成する
       </v-btn>
     </v-card-actions>
+    <div v-if="show">
+      <v-snackbar v-model="show">
+        {{ text }}
+        <v-btn color="pink" text to="/">
+          ホームへ戻る
+        </v-btn>
+      </v-snackbar>
+    </div>
   </v-card>
 </template>
 <script>
+import firebase from '~/plugins/firebase'
 export default {
   data: () => ({
     valid: true,
@@ -52,7 +55,21 @@ export default {
     postRules: [
       (v) => !!v || '文章は必須です',
       (v) => (v && v.length <= 140) || '文章は140文字以内で入力してください'
-    ]
-  })
+    ],
+    title: '',
+    content: '',
+    show: false,
+    text: '投稿が完了しました'
+  }),
+  methods: {
+    post() {
+      const db = firebase.firestore()
+      db.collection('novel').add({
+        title: this.title,
+        content: this.content
+      })
+      this.show = true
+    }
+  }
 }
 </script>
