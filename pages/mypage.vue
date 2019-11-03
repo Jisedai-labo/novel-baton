f68ac6262e041ae9cc98ce3db234d37867e1a47c
kayo289 <kayokapi@icloud.com>
2019-11-03 15:45:15 +0900

    buf

--- pages/mypage.vue
+++ pages/mypage.vue
@@ -27,14 +27,7 @@
       <v-tab-item v-for="novel in all" :key="novel">
         <v-container>
           <v-row>
-            <v-col
-              v-for="(n, index) in n"
-              :key="n"
-              cols="12"
-              sm="6"
-              md="4"
-              class="d-flex"
-            >
+            <v-col v-for="(n, index) in n" :key="index" cols="12" sm="6" md="4" class="d-flex">
               <v-card width="100%">
                 <v-card-title>{{ t.title }}</v-card-title>
                 <v-card-text>{{ t.headline }}</v-card-text>
@@ -47,28 +40,48 @@
   </v-layout>
 </template>
 <script>
+import { mapActions, mapState, mapGetters } from 'vuex'
 export default {
   data() {
     return {
       datas: [''],
       items: ['投稿履歴', 'ブックマーク一覧', 'いいね一覧'],
-      all: [[{"novel-title": "bbbbbbb","novel-headline": "aaaaaaaaaaa","novel-id":1}],[{"novel-title": "cccc","novel-headline": "dddd","novel-id":2}]],
-      histories: [{"novel-title": "dleta大冒険","novel-headline": "aaaaaaaaaaa","novel-id":1}]
+      all: [
+        [
+          {
+            'novel-title': 'bbbbbbb',
+            'novel-headline': 'aaaaaaaaaaa',
+            'novel-id': 1
+          }
+        ],
+        [{ 'novel-title': 'cccc', 'novel-headline': 'dddd', 'novel-id': 2 }]
+      ],
+      histories: [
+        {
+          'novel-title': 'dleta大冒険',
+          'novel-headline': 'aaaaaaaaaaa',
+          'novel-id': 1
+        }
+      ]
     }
-  }
-    mounted() {
-      const db = firebase.firestore()
-      const arrays = []
-      db.collection('user')
-        .get()
-        .then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-            arrays.push(doc.data())
-          })
+  },
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['isAuthenticated'])
+  },
+  mounted() {
+    const db = firebase.firestore()
+    const arrays = []
+    db.collection('user')
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          arrays.push(doc.data())
         })
-      this.datas = arrays
-      this.histories = arrays
-      this.all = [this.datas, this.datas]
-    }
+      })
+    this.datas = arrays
+    this.histories = arrays
+    this.all = [this.datas, this.datas]
+  }
 }
 </script>
