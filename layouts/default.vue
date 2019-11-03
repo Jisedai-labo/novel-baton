e7df85285752b763d888aa6779e03036c6d77d96
ryota misumi <3delta7@gmail.com>
2019-11-03 15:21:49 +0900

    ファイルを整理

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,47 +1,83 @@
 <template>
   <v-app>
     <!-- ヘッダー内容 -->
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar app>
       <v-toolbar-title>Novel Baton</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
         <v-btn text>about</v-btn>
         <v-btn text>mypage</v-btn>
-        <v-btn text>login</v-btn>
+        <v-btn v-if="isAuthenticated" text @click="logout">logout</v-btn>
+        <v-btn v-else text @click="login">login</v-btn>
       </v-toolbar-items>
     </v-app-bar>
     <v-content>
       <v-container>
+        {{ getUser }}
+        {{ isAuthenticated }}
         <nuxt />
       </v-container>
     </v-content>
 
-    <v-footer :fixed="fixed" app>
+    <v-footer app>
       <span>&copy; 201911</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mapActions, mapState, mapGetters } from 'vuex'
+import firebase from '~/plugins/firebase'
 export default {
-  data() {
-    return {
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['isAuthenticated'])
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setUser(user)
+    })
+  },
+  methods: {
+    ...mapActions(['setUser']),
+    login() {
+      const provider = new firebase.auth.TwitterAuthProvider()
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(function(result) {
+          const db = firebase.firestore()
+          const docRef = db
+            .collection('user')
+            .doc(result.credential.accessToken)
+          docRef.get().then(function(doc) {
+            if (doc.exists) {
+              docRef.update({
+                name: result.user.displayName,
+                photoURL: result.user.photoURL
+              })
+            } else {
+              const data = {
+                name: result.user.displayName,
+                photoURL: result.user.photoURL
+              }
+              db.collection('user')
+                .doc(result.credential.accessToken)
+                .set(data)
+            }
+          })
+        })
+    },
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setUser(null)
+        })
+        .catch((error) => {
+          alert(error)
+        })
     }
   }
 }
