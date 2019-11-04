543fe23d613df3fff51e976b8c230173f7dcb40a
ryota misumi <3delta7@gmail.com>
2019-11-04 19:49:01 +0900

    aaa

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -3,18 +3,26 @@
     <v-card-title class="headline">{{ title }}</v-card-title>
     <v-card-text>
       <div class="float-right">
-        <v-btn
-          v-if="isLiked"
-          text
-          class="pa-0"
-          style="min-width: auto;"
-          @click="dislike"
-        >
-          <v-icon color="pink">mdi-heart</v-icon>
-        </v-btn>
-        <v-btn v-else text class="pa-0" style="min-width: auto;" @click="like">
-          <v-icon color="pink">mdi-heart-outline</v-icon>
-        </v-btn>
+        <div v-if="isAuthenticated">
+          <v-btn
+            v-if="isLiked"
+            text
+            class="pa-0"
+            style="min-width: auto;"
+            @click="dislike"
+          >
+            <v-icon color="pink">mdi-heart</v-icon>
+          </v-btn>
+          <v-btn
+            v-else
+            text
+            class="pa-0"
+            style="min-width: auto;"
+            @click="like"
+          >
+            <v-icon color="pink">mdi-heart-outline</v-icon>
+          </v-btn>
+        </div>
       </div>
       <p class="subtitle">作成日：2019/11/02</p>
       <p class="title text--primary mt-12">文章（作成途中）</p>
@@ -90,7 +98,7 @@ export default {
     newContent: '',
     postUsers: [],
     photoURL: [],
-    isLiked: true
+    isLiked: false
   }),
   computed: {
     ...mapState(['user']),
