61565b2605dd29190f458b3472df4aea223f0963
kayo289 <kayokapi@icloud.com>
2019-11-04 17:49:47 +0900

    投稿閲覧制限

--- pages/post.vue
+++ pages/post.vue
@@ -1,47 +1,52 @@
 <template>
   <v-card class="mx-auto py-6">
-    <v-card-title class="display-2">新規投稿</v-card-title>
-    <v-card-text>
-      <p>作成日：2019/11/02</p>
-      <p class="display-1 text--primary">タイトル</p>
-      <div class="text--primary">
-        <v-text-field
-          v-model="title"
-          :rules="titleRules"
-          label="20文字以内で入力してね"
-          required
-          filled
-        />
+    <dvi v-if="isAuthenticated">
+      <v-card-title class="display-2">新規投稿</v-card-title>
+      <v-card-text>
+        <p>作成日：2019/11/02</p>
+        <p class="display-1 text--primary">タイトル</p>
+        <div class="text--primary">
+          <v-text-field
+            v-model="title"
+            :rules="titleRules"
+            label="20文字以内で入力してね"
+            required
+            filled
+          />
+        </div>
+        <p class="display-1 text--primary mt-6">文章</p>
+        <div class="text--primary">
+          <v-textarea
+            v-model="content"
+            :rules="postRules"
+            label="140文字以内で入力してね"
+            rows="10"
+            filled
+          />
+        </div>
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn raised large color="grey lighten-1 accent-4" @click="goback"
+          >戻る</v-btn
+        >
+        <v-btn raised large color="primary accent-4" @click="post"
+          >作成する</v-btn
+        >
+      </v-card-actions>
+      <div v-if="show">
+        <v-snackbar v-model="show">
+          {{ text }}
+          <v-btn color="pink" text to="/">ホームへ戻る</v-btn>
+        </v-snackbar>
       </div>
-      <p class="display-1 text--primary mt-6">文章</p>
-      <div class="text--primary">
-        <v-textarea
-          v-model="content"
-          :rules="postRules"
-          label="140文字以内で入力してね"
-          rows="10"
-          filled
-        />
-      </div>
-    </v-card-text>
-    <v-card-actions class="justify-center">
-      <v-btn raised large color="grey lighten-1 accent-4" @click="goback"
-        >戻る</v-btn
-      >
-      <v-btn raised large color="primary accent-4" @click="post"
-        >作成する</v-btn
-      >
-    </v-card-actions>
-    <div v-if="show">
-      <v-snackbar v-model="show">
-        {{ text }}
-        <v-btn color="pink" text to="/">ホームへ戻る</v-btn>
-      </v-snackbar>
+    </dvi>
+    <div v-else>
+      投稿するにはログインが必要です。
     </div>
   </v-card>
 </template>
 <script>
-import {} from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 import firebase from '~/plugins/firebase'
 export default {
   data: () => ({
@@ -60,6 +65,10 @@ export default {
     show: false,
     text: '投稿が完了しました'
   }),
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['isAuthenticated'])
+  },
   methods: {
     goback() {
       this.$router.go(-1)
