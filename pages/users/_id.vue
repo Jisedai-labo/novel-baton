a1b42607317ecadf1309c38f3039e8794908facc
kayo289 <kayokapi@icloud.com>
2019-11-04 18:54:33 +0900

    finish add link

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -29,10 +29,18 @@
                 md="4"
                 class="d-flex"
               >
-                <v-card width="100%">
-                  <v-card-title>{{ n.title }}</v-card-title>
-                  <v-card-text>{{ n.headline }}</v-card-text>
-                </v-card>
+                <v-hover v-slot:default="{ hover }">
+                  <v-card width="100%" :elevation="hover ? 6 : 2">
+                    <nuxt-link
+                      :to="n.url"
+                      class="d-block"
+                      style="height: 100%;"
+                    >
+                      <v-card-title>{{ n.title }}</v-card-title>
+                      <v-card-text>{{ n.headline }}</v-card-text>
+                    </nuxt-link>
+                  </v-card>
+                </v-hover>
               </v-col>
             </v-row>
           </v-container>
@@ -75,7 +83,7 @@ export default {
         querySnapshot.forEach(function(doc) {
           // this.novels = doc.data()
           const raw = doc.data()
-          raw.url = 'posts/' + doc.id
+          raw.url = '/posts/' + doc.id
           arrays.push(raw)
         })
       })
@@ -87,7 +95,7 @@ export default {
         querySnapshot.forEach(function(doc) {
           // this.novels = doc.data()
           const raw = doc.data()
-          raw.url = 'posts/' + doc.id
+          raw.url = '/posts/' + doc.id
           favoriteArray.push(raw)
         })
       })
